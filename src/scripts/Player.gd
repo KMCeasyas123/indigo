0104564ab3dc86b478ad5f34601cedc3926b8205
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-07 22:39:33 -0400

    Redo art and add some player sprites

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -5,6 +5,7 @@ export var deceleration = 0.75
 export var max_speed = 300
 
 var velocity = Vector2.ZERO
+var direction = 'down'
 
 func _process(_delta):
 	var input = get_input_vector()
@@ -12,7 +13,23 @@ func _process(_delta):
 
 	velocity = velocity.linear_interpolate(target_speed, acceleration if velocity.length() < target_speed.length() else deceleration)
 
+	pick_animation()
+
 	velocity = move_and_slide(velocity)
+
+func pick_animation():
+	var horizontal = abs(velocity.x) > abs(velocity.y)
+	var sprite: AnimatedSprite = $AnimatedSprite
+
+	if velocity.length() > 1:
+		if horizontal:
+			direction = 'right' if velocity.x > 0 else 'left'
+		else:
+			direction = 'down' if velocity.y > 0 else 'up'
+	
+		sprite.animation = 'walk_' + direction
+	else:
+		sprite.animation = direction
 
 func get_input_vector():
 	return Vector2(
