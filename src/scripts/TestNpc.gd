434196c3019692aa626d19fb7c42ed1fc9ae27e9
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-09 13:58:54 -0400

    Fix indentation again

--- src/scripts/TestNpc.gd
+++ src/scripts/TestNpc.gd
@@ -2,12 +2,12 @@ extends StaticBody2D
 
 func _interact(_player):
   get_node('/root/UiMainLayer').start_dialogue([
-	{
-	  'name': 'Someone',
-	  'body': 'Hey this is a pretty sweet dialogue box huh?'
-	},
-	{
-	  'name': 'Someone',
-	  'body': "But wait, there's more!"
-	}
+    {
+      'name': 'Someone',
+      'body': 'Hey this is a pretty sweet dialogue box huh?'
+    },
+    {
+      'name': 'Someone',
+      'body': "But wait, there's more!"
+    }
   ])
