584e37239c66e27a5351d1959b3f128be4bd50c6
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-11 01:44:30 -0400

    Portrait system and WIP beggar floor

---
+++ src/scripts/Beggar.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+func _interact(_player):
+  get_node('/root/UiMainLayer').start_dialogue([
+	{
+    'portrait': 'Beggar',
+	  'name': 'Old Man',
+	  'body': "You there, lively young lad. Won't you help a poor old man?"
+	},
+	{
+    'portrait': 'Beggar',
+	  'name': 'Old Man',
+	  'body': "I've been trapped in this tower for oh so long. I came here to slay the evil master, but I could never defeat the guardian of this floor."
+	},
+	{
+    'portrait': 'Beggar',
+	  'name': 'Old Man',
+	  'body': "Take my sword. You may keep it, my body is too old and frail to hold it any longer. I ask only that you free me from this trap if you can."
+	}
+  ])
--- src/scripts/DialogueBox.gd
+++ src/scripts/DialogueBox.gd
@@ -3,9 +3,27 @@ extends Control
 var tree = []
 var current_item = 0
 
+onready var dialogue_portrait: Label = $MarginContainer/HBoxContainer/VBoxContainer/ColorRect/Portrait
 onready var dialogue_name: Label = $MarginContainer/HBoxContainer/VBoxContainer/MarginContainer/Label
 onready var dialogue_body: Label = $MarginContainer/HBoxContainer/VBoxContainer2/Label2
 onready var dialogue_next: Label = $MarginContainer/HBoxContainer/VBoxContainer2/Button
+
+var portraits = {}
+
+func _ready():
+	var folder: Directory = Directory.new()
+	
+	folder.open('res://assets/portraits')
+	folder.list_dir_begin(true, true)
+	
+	var file = folder.get_next()
+	while file:
+		if file.ends_with('.png'):
+			var name = file.trim_suffix('.png')
+			portraits[name] = load('res://assets/portraits/' + name + '.png')
+		file = folder.get_next()
+
+	folder.list_dir_end()
 
 func _input(_event):
 	if Input.is_action_just_pressed('cancel') and self.visible:
@@ -24,8 +42,16 @@ func read_item():
 	if current_item >= tree.size():
 		get_node('/root/UiMainLayer').end_dialogue()
 	else:
-		dialogue_name.text = tree[current_item].name
-		dialogue_body.text = tree[current_item].body
+		var item = tree[current_item]
+		
+		dialogue_name.text = item.name
+		dialogue_body.text = item.body
+
+		if portraits[item.portrait]:
+			dialogue_portrait.texture = portraits[item.portrait]
+		else:
+			dialogue_portrait.texture = portraits['Default']
+
 		dialogue_next.visible = current_item + 1 < tree.size()
 
 func advance():
